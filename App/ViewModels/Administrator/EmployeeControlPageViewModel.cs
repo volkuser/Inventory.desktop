3e83fcb7df39c25f4e584e83df370399d12316c7
daniel demidov <demidov008h@gmail.com>
2022-10-10 00:17:40 +0300

    employee control page

--- App/ViewModels/Administrator/EmployeeControlPageViewModel.cs
+++ App/ViewModels/Administrator/EmployeeControlPageViewModel.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 using App.Models;
+using Microsoft.EntityFrameworkCore;
 using ReactiveUI;
 using Splat;
 
@@ -10,9 +14,82 @@ public class EmployeeControlPageViewModel : ViewModelBase, IRoutableViewModel
     public IScreen? HostScreen { get; }
     private ApplicationContext Db { get; set; }
 
+    private ObservableCollection<Employee> Employees { get; set; }
+
+    private Employee _selectedValue;
+    public Employee SelectedValue
+    {
+        get => _selectedValue;
+        set
+        {
+            if (_selectedValue == value) return;
+            _selectedValue = value;
+            
+            LastName = value.LastName!;
+            FirstName = value.FirstName!;
+            Email = value.Email!;
+        }
+    }
+    
+    private string? FirstName { get; set; }
+    private string? LastName { get; set; }
+    private string? Email { get; set; }
+    
+    private ICommand OnClickBtnInsert { get; set; }
+    private ICommand OnClickBtnUpdate { get; set; }
+    private ICommand OnClickBtnDelete { get; set; }
+
     public EmployeeControlPageViewModel(IScreen? screen = null)
     {
         HostScreen = screen ?? Locator.Current.GetService<IScreen>();
         Db = Singleton.GetInstance();
+
+        Employees = new ObservableCollection<Employee>(Db.Employees!);
+
+        OnClickBtnInsert = ReactiveCommand.Create(Insert);
+        OnClickBtnUpdate = ReactiveCommand.Create(Update);
+        OnClickBtnDelete = ReactiveCommand.Create(Delete);
+    }
+
+    private void Insert()
+    {
+        Employee inserting = new Employee()
+        {
+            LastName = LastName,
+            FirstName = FirstName,
+            Email = Email
+        };
+        Employees.Add(inserting);
+        Db.Employees!.Add(inserting);
+        try { Db.SaveChanges(); } catch (Exception ex) {
+            var messageBox = MessageBox.Avalonia.MessageBoxManager
+                .GetMessageBoxStandardWindow("Exception", ex.Message);
+            messageBox.Show();
+        }
+    }
+
+    private void Update()
+    {
+        SelectedValue.LastName = LastName;
+        SelectedValue.FirstName = FirstName;
+        SelectedValue.Email = Email;
+        Db.Entry(SelectedValue).State = EntityState.Modified;
+        Db.Employees!.Update(SelectedValue);
+        try { Db.SaveChanges(); } catch (Exception ex) {
+            var messageBox = MessageBox.Avalonia.MessageBoxManager
+                .GetMessageBoxStandardWindow("Exception", ex.Message);
+            messageBox.Show();
+        }
+    }
+
+    private void Delete()
+    {
+        Employees.Remove(SelectedValue);
+        Db.Employees!.Remove(SelectedValue);
+        try { Db.SaveChanges(); } catch (Exception ex) {
+            var messageBox = MessageBox.Avalonia.MessageBoxManager
+                .GetMessageBoxStandardWindow("Exception", ex.Message);
+            messageBox.Show();
+        }
     }
 }    
