0049d5e51b9dbcbb00031ee3338de9200e10b3ba
daniel demidov <demidov008h@gmail.com>
2022-10-10 04:11:40 +0300

    uquipment types control page

--- App/ViewModels/CommissionMember/EquipmentTypeControlViewModel.cs
+++ App/ViewModels/CommissionMember/EquipmentTypeControlViewModel.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 using App.Models;
 using ReactiveUI;
 using Splat;
@@ -9,10 +12,74 @@ public class EquipmentTypeControlViewModel : ViewModelBase, IRoutableViewModel
     public string UrlPathSegment => "equipmentType";
     public IScreen? HostScreen { get; }
     private ApplicationContext Db { get; set; }
+    
+    private ObservableCollection<EquipmentType> EquipmentTypes { get; set; }
+
+    private EquipmentType _selectedValue;
+    public EquipmentType SelectedValue
+    {
+        get => _selectedValue;
+        set
+        {
+            if (_selectedValue == value) return;
+            _selectedValue = value;
+            
+            Name = value.Name!;
+        }
+    }
+    
+    private string? Name { get; set; }
+    
+    private ICommand OnClickBtnInsert { get; set; }
+    private ICommand OnClickBtnUpdate { get; set; }
+    private ICommand OnClickBtnDelete { get; set; }
 
     public EquipmentTypeControlViewModel(IScreen? screen = null)
     {
         HostScreen = screen ?? Locator.Current.GetService<IScreen>();
         Db = Singleton.GetInstance();
+        
+        EquipmentTypes = new ObservableCollection<EquipmentType>(Db.EquipmentTypes!);
+
+        OnClickBtnInsert = ReactiveCommand.Create(Insert);
+        OnClickBtnUpdate = ReactiveCommand.Create(Update);
+        OnClickBtnDelete = ReactiveCommand.Create(Delete);
+    }
+    
+    private void Insert()
+    {
+        EquipmentType inserting = new EquipmentType()
+        {
+            Name = Name
+        };
+        EquipmentTypes.Add(inserting);
+        Db.EquipmentTypes!.Add(inserting);
+        try { Db.SaveChanges(); } catch (Exception ex) {
+            var messageBox = MessageBox.Avalonia.MessageBoxManager
+                .GetMessageBoxStandardWindow("Exception", ex.Message);
+            messageBox.Show();
+        }
+    }
+
+    private void Update()
+    {
+        SelectedValue.Name = Name;
+        Db.EquipmentTypes!.Update(SelectedValue);
+        try { Db.SaveChanges(); } catch (Exception ex) {
+            var messageBox = MessageBox.Avalonia.MessageBoxManager
+                .GetMessageBoxStandardWindow("Exception", ex.Message);
+            messageBox.Show();
+        }
+    }
+
+    private void Delete()
+    {
+        EquipmentTypes.Remove(SelectedValue);
+        Db.EquipmentTypes!.Remove(SelectedValue);
+        try { Db.SaveChanges(); } catch (Exception ex) {
+            var messageBox = MessageBox.Avalonia.MessageBoxManager
+                .GetMessageBoxStandardWindow("Exception", ex.Message);
+            messageBox.Show();
+        }
     }
 }    
